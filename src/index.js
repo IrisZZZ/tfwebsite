3449ff91420dd442dd0be401a431df410c017c6e
Iris Zheng <33638345+IrisZZZ@users.noreply.github.com>
2018-11-18 13:21:30 -0800

    update link
    update

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL>
     <Login />
   </BrowserRouter>,
   document.getElementById("root")
